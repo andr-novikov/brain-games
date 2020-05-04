9acd12f8b72cb70c5def98f78f2495cd2aa21db6
Andrey Novikov <anovikovrf@gmail.com>
2020-05-04 23:47:04 +0300

    change logic of game engine

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
-/* eslint no-restricted-syntax: ["off", "ForOfStatement"] */
 import readlineSync from 'readline-sync';
 
-export default (nameGame) => {
+export default (description, generateQuestionAndAnswer) => {
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
-
-  console.log(nameGame('description'));
+  console.log(description);
 
   const numberOfGameRounds = 3;
+
   for (let i = 0; i < numberOfGameRounds; i += 1) {
-    const [question, correctAnswer] = nameGame();
+    const [question, correctAnswer] = generateQuestionAndAnswer();
     console.log(`Question: ${question}`);
     const getAnswer = readlineSync.question('Your answer: ');
 
